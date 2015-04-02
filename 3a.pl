b0f8dc8e85900dd7a81f09face96eeeaf0f4ee45
danben4 <danielbenediktsson4@gmail.com>
2015-04-02 17:35:09 +0000

    flot

--- 3a.pl
+++ 3a.pl
@@ -7,6 +7,10 @@
 /*
  * Facts:
  */
+ mymerge([], Y, Y).
+ mymerge(X, [], X).
 /*
  * Rules:
  */
+ mymerge([X|Xs], [Y|Ys], [X|Z]) :- X < Y, mymerge(Xs, [Y|Ys], Z).
+ mymerge([X|Xs], [Y|Ys], [Y|Z]) :- X >= Y, mymerge([X|Xs], Ys, Z).
