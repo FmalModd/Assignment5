2d47c1bb624354ebfd8a4d50ef3767054742a888
danben4 <danielbenediktsson4@gmail.com>
2015-04-02 14:19:26 +0000

    flot

--- 1.pl
+++ 1.pl
@@ -2,11 +2,39 @@
  * Problem 1
  */
 /**
- * a
+ * a)
  */
 /*
- * b
+ * Facts:
+ */
+affair('Steve', 'Jane').
+affair('Jane', 'Steve').
+affair('Mary', 'Steve').
+affair('Steve', 'Mary').
+married('Tom', 'Jane').
+married('Jane', 'Tom').
+rich('Steve').
+greedy('John').
+victim('Steve').
+/*
+ * Rules:
+ */
+hates(L, M) :- married(L, S), affair(S, M).
+greed(L, M) :- greedy(L), not(rich(L)), rich(M).
+murders(L, M) :- hates(L, M).
+murders(L, M) :- greed(L, M).
+suspect(L) :- murders(L, M), victim(M).
+/*
+ * b)
+ */
+/*
+ * Suspect 1: 'Tom'.
+ * Suspect 2: 'John'.
  */
 /**
- * c
+ * c)
  */
+/*
+ * rich('John').
+ */
+
