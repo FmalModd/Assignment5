6bdf1cc3dd4d1f43581be1ee371aa371f4257482
danben4 <danielbenediktsson4@gmail.com>
2015-04-02 17:11:17 +0000

    flot

--- 2e.pl
+++ 2e.pl
@@ -7,6 +7,9 @@
 /*
  * Facts:
  */
+ addUpListHelper([], [], _).
 /*
  * Rules:
  */
+ addUpListHelper([X|Xs], [Y|Ys], Acc) :- Y is Acc + X, addUpListHelper(Xs, Ys, Y).
+ addUpList(X, Y) :- addUpListHelper(X, Y, 0).
