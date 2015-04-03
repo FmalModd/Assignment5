6d170317180e8b210d6a829b38f87b07a254213d
Jorundur <jorundurj@gmail.com>
2015-04-03 11:02:58 +0000

    4b

--- 4b.pl
+++ 4b.pl
@@ -4,3 +4,12 @@
 /**
  * b)
  */
+/*
+ * Facts:
+ */
+ preorder(nil, []).
+ preorder(t(X, nil, nil), [X]).
+/*
+ * Rules:
+ */
+ preorder(t(X, L, R), [X|Xs]) :- preorder(L, Ls), preorder(R, Rs), append(Ls, Rs, Xs).
