a2fc72c21e9b807de0a83e070e9e76e61d6ddb5e
Jorundur <jorundurj@gmail.com>
2015-04-03 12:47:55 +0000

    4c final

--- 4c.pl
+++ 4c.pl
@@ -12,4 +12,6 @@
 /*
  * Rules:
  */
- leaves(t(_,L,R),Xs) :- leaves(L,Ls), leaves(R,Rs), append(Ls,Rs,Xs).
+ leaves(t(_,L,nil), Xs) :- not(L = nil), leaves(L, Xs).
+ leaves(t(_,nil,R), Xs) :- not(R = nil), leaves(R, Xs).
+ leaves(t(_,L,R), Xs) :- not(L = nil), not(R = nil), leaves(L,Ls), leaves(R,Rs), append(Ls,Rs,Xs).
