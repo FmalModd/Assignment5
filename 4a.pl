11bdc360b8940fb196ed769e75087f779082d2ac
Jorundur <jorundurj@gmail.com>
2015-04-03 13:41:56 +0000

    4a fixed

--- 4a.pl
+++ 4a.pl
@@ -12,4 +12,6 @@
 /*
  * Rules:
  */
- binaryTree(t(_, L, R)) :- binaryTree(L), binaryTree(R).
+ binaryTree(t(_, L, nil)) :- not(L = nil), binaryTree(L).
+ binaryTree(t(_, nil, R)) :- not(R = nil), binaryTree(R).
+ binaryTree(t(_, L, R)) :- not(L = nil), not(R = nil), binaryTree(L), binaryTree(R).
