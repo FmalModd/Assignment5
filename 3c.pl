188f3c456135f39f06d14680c60faa9a706378e5
danben4 <danielbenediktsson4@gmail.com>
2015-04-03 10:41:55 +0000

    flot

--- 3c.pl
+++ 3c.pl
@@ -7,8 +7,32 @@
 /*
  * Facts:
  */
- mysort([], []).
+ mymerge([], Y, Y).
+ mymerge(X, [], X).
 /*
  * Rules:
  */
- mysort() :- mysort().
+ mymerge([X|Xs], [Y|Ys], [X|Z]) :- X < Y, mymerge(Xs, [Y|Ys], Z).
+ mymerge([X|Xs], [Y|Ys], [Y|Z]) :- X >= Y, mymerge([X|Xs], Ys, Z).
+/*
+ * Facts:
+ */
+ mysplit([],[],[]).
+ mysplit([X], [X], []).
+ mysplit([X,Y], [X], [Y]).
+/*
+ * Rules:
+ */
+ mysplit([X1,X2|Xs], [X1|Y], [X2|Z]) :- mysplit(Xs, Y, Z).
+/*
+ * Facts:
+ */
+ mysort([], []).
+ mysort([X], [X]).
+/*
+ * Rules:
+ */
+ /*
+  * TODO RETURN ONLY ONE RESULT
+  */
+ mysort(X,Y) :- mysplit(X,L1,L2), mysort(L1, S1), mysort(L2, S2), merge(S1,S2,Y).
