335d39260336fbc4f30b9774853ce461fd729fe5
Jorundur <jorundurj@gmail.com>
2015-04-03 14:06:00 +0000

    4b

--- 4b.pl
+++ 4b.pl
@@ -8,8 +8,9 @@
  * Facts:
  */
  preorder(nil, []).
- preorder(t(X, nil, nil), [X]).
 /*
  * Rules:
  */
- preorder(t(X, L, R), [X|Xs]) :- preorder(L, Ls), preorder(R, Rs), append(Ls, Rs, Xs).
+preorder(t(X,L,nil), [X|Xs]) :- not(L = nil), preorder(L, Ls), append(Ls, Xs).
+preorder(t(X,nil,R), [X|Xs]) :- not(R = nil), preorder(R, Rs), append(Rs, Xs).
+preorder(t(X, L, R), [X|Xs]) :- preorder(L, Ls), preorder(R, Rs), append(Ls, Rs, Xs).
