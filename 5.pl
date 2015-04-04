9e77e464b0d9289a7f251de7655e991dc1753a57
danben4 <danielbenediktsson4@gmail.com>
2015-04-04 17:37:32 +0000

    flot

--- 5.pl
+++ 5.pl
@@ -2,11 +2,17 @@
  * Problem 5
  */
 /*
- * Facts:
+ * Rules:
  */
- factor(X) :- number(X).
- factor(X) :- ['('], expr(X), [')'].
+ expr(X) :- term(X).
+ expr([X1,X2|Xs]) :- term([X1]), X2 == +, expr(Xs).
 /*
  * Rules:
  */
-
+ term(X) :- factor(X).
+ term([X1,X2|Xs]) :- factor([X1]), X2 == *, term(Xs).
+/*
+ * Rules:
+ */
+ factor([X]) :- number(X).
+ factor(['('|X]) :- append(Y,[')'],X), expr(Y).
