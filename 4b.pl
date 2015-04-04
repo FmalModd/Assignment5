db270f6b0a0eba3785aaa89f4758615d486b9764
danben4 <danielbenediktsson4@gmail.com>
2015-04-04 17:39:17 +0000

    flot

--- 4b.pl
+++ 4b.pl
@@ -11,6 +11,6 @@
 /*
  * Rules:
  */
-preorder(t(X,L,nil), [X|Xs]) :- not(L = nil), preorder(L, Ls), append(Ls, Xs).
-preorder(t(X,nil,R), [X|Xs]) :- not(R = nil), preorder(R, Rs), append(Rs, Xs).
-preorder(t(X, L, R), [X|Xs]) :- preorder(L, Ls), preorder(R, Rs), append(Ls, Rs, Xs).
+ preorder(t(X,L,nil), [X|Xs]) :- not(L = nil), preorder(L, Ls), append(Ls, Xs).
+ preorder(t(X,nil,R), [X|Xs]) :- not(R = nil), preorder(R, Rs), append(Rs, Xs).
+ preorder(t(X, L, R), [X|Xs]) :- preorder(L, Ls), preorder(R, Rs), append(Ls, Rs, Xs).
